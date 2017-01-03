2c71c3336557fd8df9d56626ed5386647b73b146
Hack Reactor Students <accounts@hackreactor.com>
2017-01-02 20:34:33 -0800

    Implement room fetching.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,7 +3,7 @@
 var escapeHtml = function(str) {
   var div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
+  return div.innerHTML.replace(/'/g, '"');
 };
 
 class Client {
@@ -11,13 +11,19 @@ class Client {
   constructor() {
     this.server = 'https://api.parse.com/1/classes/messages';
     this.lastTimeStamp = undefined;
+    this.currentRoom = 'lobby';
   }
 
   init () {
     $('.submit').on('click', this.handleSubmit.bind(this));
+    $('#roomSelect').on('change', this.changeRoom.bind(this));
     $('#chats').on('click', '.username', this.handleUsernameClick);
     this.fetchLatest();
     setInterval(this.fetchLatest.bind(this), 1000);
+    this.fetchRoomList();
+    setInterval(this.fetchRoomList.bind(this), 3000);
+    // this.fetchRoom('jamesai');
+    // setInterval(this.fetchRoom.bind(this, 'jamesai'), 1000);
   }
 
   send (message) {
@@ -53,13 +59,47 @@ class Client {
     });
   }
 
+  // fetchRoom (room) {
+  //   var time = this.lastTimeStamp;
+  //   $.ajax({
+  //     // This is the url you should use to communicate with the parse API server.
+  //     url: this.server,
+  //     type: 'GET',
+  //     data: {'where': `{"roomname":"${room}"}`, 'order': 'updatedAt'}, 
+  //     contentType: 'application/json',
+  //     success: this.displayMessages.bind(this),
+  //     error: function (data) {
+  //       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+  //       console.error('chatterbox: Failed to receive message', data);
+  //     }
+  //   });
+  // }
+
+  fetchRoomList (room) {
+    if (!$('#roomSelect').is(':focus')) {
+      $.ajax({
+        // This is the url you should use to communicate with the parse API server.
+        url: this.server,
+        type: 'GET',
+        data: {'keys': 'roomname', 'order': 'updatedAt', 'limit': '1000'}, 
+        contentType: 'application/json',
+        success: this.updateRoomList.bind(this),
+        error: function (data) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to receive message', data);
+        }
+      });
+    }
+    
+  }
   fetchLatest () {
     var time = this.lastTimeStamp;
+    var room = this.currentRoom;
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: this.server,
       type: 'GET',
-      data: time ? {'where': `{"updatedAt":{"$gt":"${time}"}}`, 'order': 'updatedAt'} : {'order': '-updatedAt'}, 
+      data: time ? {'where': `{"updatedAt":{"$gt":"${time}"}, "roomname":"${room}"}`, 'order': 'updatedAt'} : {'where': `{"roomname":"${room}"}`, 'order': '-updatedAt'}, 
 
       // data: {"order":"-createdAt", "group": "chatroom"},
       contentType: 'application/json',
@@ -83,20 +123,29 @@ class Client {
   renderRoom(room) {
     $('#roomSelect').append(`<option value=${room}>${room}</option>`);
   }
+  updateRoomList (data) {
+    var renderRoomFunc = this.renderRoom;
+    _.each(data.results, function (item) {
+      if ($(`#roomSelect option:contains('${escapeHtml(item.roomname)}')`).length === 0) {
+        renderRoomFunc(escapeHtml(item.roomname));
+      }
+    });
+
+
+  }
 
   displayMessages (data) {
     var renderFunc = this.renderMessage;
     if (!this.lastTimeStamp) { // first time we are retrieving messages
       data.results.reverse();  // messages are in reverse chronological order, we want chronological
     }
-    // console.log(data);
     _.each(data.results, function (item) {
       renderFunc(item);  
     });
     if (data.results.length > 0) {
       this.lastTimeStamp = data.results[data.results.length - 1].updatedAt;
+      $('#chats').scrollTop($('#chats')[0].scrollHeight);
     }
-    // console.log(this.lastTimeStamp);
   }
 
 
@@ -107,12 +156,17 @@ class Client {
   handleSubmit () {  
     var message = {
       username: window.location.search.split('=')[1],
-      roomname: 'lobby',
+      roomname: this.currentRoom,
       text: $('#message').val()
     }; 
     this.send(message);
   }
 
+  changeRoom () {
+    this.currentRoom = $('#roomSelect').val();
+    this.lastTimeStamp = undefined;
+    this.clearMessages();
+  }
 }
 
 $(document).ready(function() {
